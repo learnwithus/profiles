3e80ab28fd77a8120dba818035295e2c7c5305cd
Jun Hyung Park <junhyungandrewpark@gmail.com>
2016-05-26 22:12:45 -0700

    implement search by name and title

--- php/config.php
+++ php/config.php
@@ -1,6 +1,7 @@
 <?php
-	$host = "localhost";
-	$username = "nomonke1_park";
-	$password = "123...Park"; 
-	$db_name = "nomonke1_display_profile";
+    $host = "localhost";
+    $username = "nomonke1_park";
+    $password = "123...Park"; 
+    $db_name = "nomonke1_display_profile";
 ?>
+
